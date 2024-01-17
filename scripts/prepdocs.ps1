e9c79be314df22da3ccc291d5a141ecb4cf0b1b0
Sumit Malik <sumitmalik51@gmail.com>
2024-01-17 17:40:05 +0530

    Update prepdocs.ps1

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -1,19 +1,3 @@
-Write-Host ""
-Write-Host "Loading azd .env file from current environment"
-Write-Host ""
-
-$output = azd env get-values
-
-foreach ($line in $output) {
-  if (!$line.Contains('=')) {
-    continue
-  }
-
-  $name, $value = $line.Split("=")
-  $value = $value -replace '^\"|\"$'
-  [Environment]::SetEnvironmentVariable($name, $value)
-}
-
 Write-Host "Environment variables set."
 
 $pythonCmd = Get-Command python -ErrorAction SilentlyContinue
