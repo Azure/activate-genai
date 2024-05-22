ce281a139a7d73f8a66bb4a5f2296f5d9299d601
Sumit Malik <sumitmalik51@gmail.com>
2024-05-22 23:13:55 +0530

    Update prepdocs.ps1

--- scripts/prepdocs.ps1
+++ scripts/prepdocs.ps1
@@ -1,7 +1,6 @@
 Write-Host "Environment variables set."
 
-choco install rust -y
-choco install azd -y
+
 
 rustc --version
 
@@ -12,7 +11,7 @@ if (-not $pythonCmd) {
   # fallback to python3 if python not found
   $pythonCmd = Get-Command python3 -ErrorAction SilentlyContinue
 }
-
+refreshenv
 Write-Host 'Creating python virtual environment "scripts/.venv"'
 Start-Process -FilePath ($pythonCmd).Source -ArgumentList "-m venv ./scripts/.venv" -Wait -NoNewWindow
 
@@ -22,7 +21,38 @@ if (Test-Path -Path "/usr") {
   $venvPythonPath = "./scripts/.venv/bin/python"
 }
 
-write-host "Enter your resource group name where your terraform infra is deployed"
+$rgname = Read-host "Enter your resource group name where your terraform infra is deployed"
+$rgname
+$location = Read-Host "Enter your activate genai resource group location"
+$subid = Read-Host "Enter your SubID"
+
+cd C:\Users\demouser\activate-genai\scripts
+mkdir genai
+cd genai
 
 
-azd env new activategenai
+
+azd init -t https://github.com/Azure-Samples/azure-search-openai-demo -e activate-genai -s $subid -l $location
+
+$envContent = "AZURE_RESOURCE_GROUP=`"$rgname`""
+$envContent1 = "AZURE_LOCATION=`"$location`""
+$envContent2 = "AZURE_SUBSCRIPTION_ID=`"$subid`""
+
+
+
+
+
+# Specify the path to the .env file
+$envFilePath = "C:\azure-search-openai-demo\.azure\activegenai\.env"
+
+# Add content to the file
+Add-Content -Path $envFilePath -Value $envContent
+Add-Content -Path $envFilePath -Value $envContent1
+Add-Content -Path $envFilePath -Value $envContent2
+Add-Content -Path $envFilePath -Value $envContent3
+
+azd auth login
+
+azd up -e activate-genai
+
+
